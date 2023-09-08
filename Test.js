d250fd4a83870bef5f60c0f7d02cbb40f270c77a
Jett Pennington <jettpennington@gmail.com>
2023-09-08 16:39:19 +0000

    test to show nathan while loops

---
+++ Test.js
@@ -0,0 +1,18 @@
+var going = 9;
+var on = 3;
+
+if(going < on){
+    while (going < on){
+        console.log(on);
+        on = on -1
+    };
+} else {
+    while (going > on){
+        console.log(on);
+        on = on +1
+    };
+};
+
+
+
+console.log("Here!")
